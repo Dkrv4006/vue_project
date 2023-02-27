7060cb807a5bff979500beb9f5878945c9aed683
Dkrv4006 <danielkennedy400619@gmail.com>
2023-02-27 05:55:04 -0300

    Update Wallet.vue

--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -1,60 +1,93 @@
 <template>
-    <div>
-      <div v-for="coin in coins" :key="coin.id">
-        <h2>{{ coin.name }} ({{ coin.symbol }})</h2>
-        <p>Preço: {{ coin.price }}</p>
-        <img :src="coin.logo" alt="Logo" width="50">
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        coins: [],
-      };
+  <div>
+    <highcharts class="hc" :options="chartOptions" ref="mapChart"></highcharts>
+  </div>
+</template>
+
+<script>
+import Highcharts from 'highcharts'
+import exportingInit from 'highcharts/modules/exporting'
+
+exportingInit(Highcharts)
+
+export default {
+  data() {
+    return {
+      dat: []
+    };
+  },
+
+  computed: {
+    chartOptions(){
+      return{
+        chart: {
+        type: 'area',
+        width: 800,
+        backgroundColor: '#000000'
     },
-    // async created() {
-    //   const apiKey = '3d1fac3f-7cae-48cd-994d-7c085aba9619';
-    //   const symbols = 'BTC,ETH';
-    //   const limit = 2;
-    //   const response = await axios.get(`https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`, {
-    //     headers: {
-    //       'X-CMC_PRO_API_KEY': apiKey,
-    //     },
-    //   });
-    //   const data = response.data.data;
-    //   console.log(data);
-    //   this.coins = Object.keys(data).map(key => ({
-    //     id: data[key].id,
-    //     name: data[key].name,
-    //     symbol: data[key].symbol,
-    //     price: data[key].quote.USD.price,
-    //     logo: `https://s2.coinmarketcap.com/static/img/coins/64x64/${data[key].id}.png`,
-    //   }));
-//     new Promise(async (resolve, reject) => {
-//   try {
-//     response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
-//       headers: {
-//         "X-CMC_PRO_API_KEY": '3d1fac3f-7cae-48cd-994d-7c085aba9619',
-//       },
-//     });
-//   } catch(ex) {
-//     response = null;
-//     // error
-//     console.log(ex);
-//     reject(ex);
-//   }
-//   if (response) {
-//     // success
-//     const json = response.data;
-//     console.log(json);
-//     resolve(json);
-//   }
-// });
-//     },
-  };
-  </script>
+    accessibility: {
+        description: 'Image description: An area chart compares the nuclear stockpiles of the USA and the USSR/Russia between 1945 and 2017. The number of nuclear weapons is plotted on the Y-axis and the years on the X-axis. The chart is interactive, and the year-on-year stockpile levels can be traced for each country. The US has a stockpile of 6 nuclear weapons at the dawn of the nuclear age in 1945. This number has gradually increased to 369 by 1950 when the USSR enters the arms race with 6 weapons. At this point, the US starts to rapidly build its stockpile culminating in 32,040 warheads by 1966 compared to the USSR’s 7,089. From this peak in 1966, the US stockpile gradually decreases as the USSR’s stockpile expands. By 1978 the USSR has closed the nuclear gap at 25,393. The USSR stockpile continues to grow until it reaches a peak of 45,000 in 1986 compared to the US arsenal of 24,401. From 1986, the nuclear stockpiles of both countries start to fall. By 2000, the numbers have fallen to 10,577 and 21,000 for the US and Russia, respectively. The decreases continue until 2017 at which point the US holds 4,018 weapons compared to Russia’s 4,500.'
+    },
+    title: {
+        text: 'US and USSR nuclear stockpiles'
+    },
+    subtitle: {
+        text: 'Source: <a href="https://fas.org/issues/nuclear-weapons/status-world-nuclear-forces/" ' +
+            'target="_blank">FAS</a>'
+    },
+    xAxis: {
+      gridZIndex:0,
+      crosshair: true,
+        categories: this.list.map(i => i.element),
+        labels: {
+            formatter: function () {
+                return this.value // clean, unformatted number for year
+            }
+        },
+        accessibility: {
+            rangeDescription: 'Range: 1940 to 2017.'
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Nuclear weapon states'
+        },
+        labels: {
+            formatter: function () {
+                return this.value  + 'k';
+            }
+        }
+    },
+    tooltip: {
+        pointFormat: '{series.name} had stockpiled <b>{point.y}</b><br/>warheads in {point.x}',
+        backgroundColor: '#000000'
+    },
+    plotOptions: {
+        area: {
+            pointStart:1,
+            marker: {
+                enabled: false,
+                symbol: 'circle',
+                radius: 1,
+                states: {
+                    hover: {
+                        enabled: true
+                    }
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'USA',
+        data: this.list.map(i => Number(i.openprice))
+    }]
+    }
+  },
+  list(){
+    console.log(this.$store.state.api.data);
+    return this.$store.state.api.data
+
+  }
+  }//computed
+};
+</script>
