09e08d93c7ae9c487111edc48416d7a1512cbcf8
Dkrv4006 <danielkennedy400619@gmail.com>
2023-03-04 10:07:58 -0300

    Update api.js

--- src/store/modules/api.js
+++ src/store/modules/api.js
@@ -2,7 +2,8 @@ import axios from "axios"
 
 const state = {
   data: [''],
-  coin: ['']
+  coin: [''],
+  mony: ['']
 
 }
 
@@ -16,127 +17,80 @@ const mutations = {
     console.log(coin);
      
   },
+  setMony(state,mony){
+    state.mony = mony
+    console.log(mony);
+     
+  },
 
 }
 
 const actions = {
+
   async  fetchData(commit, payload) {
 
+    const symbols = []
+    const interval = '1M';
+    const limit = 12;
+    const startTime = new Date().setFullYear(new Date().getFullYear() - 1); // data de um ano atrás
+    const endTime = Date.now(); // data atual
 
-    
-  //   const moedasUSDT = dat.filter(item => item.symbol.slice(-4) === 'USDT');
-
-    const symbols = []// lista de símbolos das moedas que você deseja obter o histórico
-const interval = '1M'; // intervalo de tempo mensal
-const limit = 12; // número máximo de resultados (12 meses)
-const startTime = new Date().setFullYear(new Date().getFullYear() - 1); // data de um ano atrás
-const endTime = Date.now(); // data atual
-console.log(startTime);
-console.log(endTime);
-if(payload !== undefined){
-  symbols.push(payload)
-}else{
-  console.log(this.setData);
-  symbols.push('BTCUSDT')
-}
-
-commit.commit('setCoin',symbols)
-const requests = symbols.map(symbol => {
-  const url = `https://api.binance.com/api/v3/klines?symbol=${symbols}&interval=${interval}&limit=${limit}&startTime=${startTime}&endTime=${endTime}`;
-  return axios.get(url);
-});
-
-
-      try {
-        const responses = await Promise.all(requests);
-        const data = responses.map((response) => {
-          const items = response.data;
-      
-          const newData = items.map((item) => ({
-            element: new Date(item[0]).toLocaleDateString('pt-BR'),
-            openprice: item[1],
-            maxprice: item[2],
-            minprice: item[3],
-            closeprice: item[4],
-          }));
-      
-          return newData;
-        }).flat();
-      
-  
-        // console.table(data);
-        commit.commit('setData' , data)
-      } catch (error) {
-        console.error(error);
-      }
-    },
-
-//     const symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT'] // lista de símbolos de moeda desejados
-  async  fetch(commit, payload) {
-
-   const dat = await  axios.get('https://api.binance.com/api/v3/ticker/24hr')
-    .then(response => response.data)
-    .catch(error => {
-      console.error(error)
-    })
-    const data = new Date().getTime()
-    const dataFormatada = new Date(data).toLocaleDateString('pt-BR')
-    console.log(Date.now())
-    
-    
-    const moedasUSDT = dat.filter(item => item.symbol.slice(-4) === 'USDT');
-    // commit.commit('setData' , moedasUSDT)
-
-
-
-    console.log(moedasUSDT );
-
-
+    if(payload !== undefined){
+      symbols.push(payload)
+    }else{
+      console.log(this.setData);
+      symbols.push('BTCUSDT')
     }
 
-//     const symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT'] // lista de símbolos de moeda desejados
+    commit.commit('setCoin',symbols) 
 
-// const startTime = 1640995200000 // carimbo de data/hora Unix em milissegundos para o início do período desejado (01/01/2022)
-// const endTime = Date.now() // carimbo de data/hora Unix em milissegundos para o fim do período desejado (31/01/2022)
-
-// const interval = '1M' // intervalo de tempo desejado (mensal)
-
-// const fetchData = async (symbol) => {
-//   const url = `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&startTime=${startTime}&endTime=${endTime}`
-//   const response = await axios.get(url)
-//   const data = response.data
-//   console.log(`Histórico de ${symbol}:`)
-//   console.log(data)
-// }
-
-// symbols.forEach(fetchData)
+    const requests = symbols.map(symbol => {
+      const url = `https://api.binance.com/api/v3/klines?symbol=${symbols}&interval=${interval}&limit=${limit}&startTime=${startTime}&endTime=${endTime}`;
+      return axios.get(url);
+    });
 
 
-
-
-
-
-
-
-  //  const dat = await  axios.get('https://api.binance.com/api/v3/ticker/24hr')
-  //   .then(response => response.data)
-  //   .catch(error => {
-  //     console.error(error)
-  //   })
-  //   const data = new Date().getTime()
-  //   const dataFormatada = new Date(data).toLocaleDateString('pt-BR')
-  //   console.log(Date.now())
+    try {
+      const responses = await Promise.all(requests);
+      const data = responses.map((response) => {
+        const items = response.data;
     
+        const newData = items.map((item) => ({
+          element: new Date(item[0]).toLocaleDateString('pt-BR'),
+          openprice: item[1],
+          maxprice: item[2],
+          minprice: item[3],
+          closeprice: item[4],
+        }));
     
-  //   const moedasUSDT = dat.filter(item => item.symbol.slice(-4) === 'USDT');
-  //   commit.commit('setData' , moedasUSDT)
+        return newData;
+      }).flat();
+
+      commit.commit('setData' , data)
+    } catch (error) {
+        console.error(error);
+      }
+  },
+
+
+  async mony(commit, payload) {
+
+    const mony = await axios.get('https://api.binance.com/api/v3/ticker/24hr')
+                            .then(response => response.data)
+                            .catch(error => {
+                              console.error(error)
+                            })
+      const date = new Date().getTime()
+      const dataFormatada = new Date(date).toLocaleDateString('pt-BR')
+      
+      const moedasUSDT = mony.filter(item => item.symbol.slice(-4) === 'USDT' && item.symbol.length <= 8);
+      commit.commit('setMony' , moedasUSDT)
 
 
 
-    // console.log(moedasUSDT );
-    
-  }
-
+      // console.log(moedasUSDT );
+  }  
+}
 export default {
   state,
   mutations,
